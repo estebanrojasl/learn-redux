9fb8517d11b792c3ad2f2e2283f82fd386ba71c8
esteban7590 <esteban7590@hotmail.com>
2020-10-27 14:23:30 -0500

    simple counter

--- src/index.js
+++ src/index.js
@@ -4,6 +4,44 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { createStore } from "redux";
+
+//Action
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+//Reducer
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+
+    case "DECREMENT":
+      return state - 1;
+
+    default:
+      return state;
+  }
+};
+
+//Store
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+//Dispatch
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
